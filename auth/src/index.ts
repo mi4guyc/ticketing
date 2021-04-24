0d62513d2c2a42fd2539f8a64980ef088db2e0d9
mi4guyc <46153732+mi4guyc@users.noreply.github.com>
2021-04-24 04:31:22 -0700

    try out push again!

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("Auth-Starting up");
+  console.log("Auth-Starting up...");
 
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
